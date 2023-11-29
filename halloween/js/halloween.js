46f12d44759d6784c192562cb1ba38207af0bcab
Miguel-Angel-Bonilla <m.a.boni26@hotmail.com>
2023-11-30 00:35:09 +0100

    Final version

--- halloween/js/halloween.js
+++ halloween/js/halloween.js
@@ -1,22 +1,20 @@
-/**********************************************************
- * 
- * 
- * 
- */
-
+/*******************************************
+* Author: Miguel Ángel Bonilla Fernández   *
+* File: JavaScript (Halloween)             *
+********************************************/
+/*SHOWS AN ELEMENT*/
 function show(element){
     document.getElementById(element).style.visibility="visible";
-    /*extrae un elemento y cambia algo en su estilo*/
 }
-
+/*HIDES AN ELEMENT*/
 function hide(element){
     document.getElementById(element).style.visibility="hidden";
 }
-
+/*PLAYS SOUND*/
 function sound(element){
     document.getElementById(element).play();
 }
-
+/*PAUSES SOUND*/
 function silence(element){
     document.getElementById(element).pause();
 }
